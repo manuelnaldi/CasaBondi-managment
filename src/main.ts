c0d2ab9733bf595402bf687a2f9dc5989cd066c9
Manuel <mnaldi.business@gmail.com>
2023-05-07 20:00:19 +0200

    feat: add routes code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Calendar from './components/Calendar.vue';
+import Contacts from './components/Contacts.vue';
+import Home from './components/Home.vue';
 
-createApp(App).mount('#app')
+const home = Home;
+const calendar = Calendar;
+const contacts = Contacts;
+
+const routes = [
+  { path: '/', component: home },
+  { path: '/calendar', component: calendar },
+  { path: '/contacts', component: contacts }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')
