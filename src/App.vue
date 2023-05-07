a8fc9f2aeb06c80ed5b3f0c3d11f1a0b6aa24fb6
Manuel <mnaldi.business@gmail.com>
2023-05-07 20:00:42 +0200

    feat: add new layout with navbar WIP

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
 </script>
 
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/calendar">Calendario</router-link>
-  <router-link to="/contacts">Contatti</router-link>
-  <router-view></router-view>
+    <div class="navbar">
+      <img class="logo" src="/casabondi.svg"/>
+      <a><router-link to="/">Home</router-link></a>
+      <a><router-link to="/calendar">Calendario</router-link></a>
+      <a><router-link to="/contacts">Contatti</router-link></a>
+    </div>
+    <br>
+    <div class="view">
+      <router-view></router-view>
+    </div>
 </template>
 
 
