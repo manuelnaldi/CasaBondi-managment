58efb559dd1d58f710b1150dbe95fd6823ed27a7
Manuel <mnaldi.business@gmail.com>
2023-05-07 20:23:33 +0200

    feat: WIP Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,24 @@
 <template>
+    <div class="card">
     <h1>Benvenuto!</h1>
-    <p>Questa è la home.</p>
+    <p>Sito web creato per i volontari della bibilioteca di Castenaso che vogliono consentire 
+    la fruizione della biblioteca negli orari in cui dovrebbe rimanere chiusa.
+    Il progetto in questo momendo è in fase di sviluppo è andrà provato, con la partecipazione
+    di tutti!</p>
+    <br>
+    <p>Nella sezione <bold>calendario</bold>è possibile accedere per consultare i diversi orari di disponibilità!</p>
+    <br>
+    <p>Nella sezione contatti puoi trovare il link al gruppo whatsapp per comunicare le tue disponibilità
+    che verranno aggiunte nel calendario su questo sito web.</p>
+</div>
 </template>
+
+<style>
+.card{
+    padding: 1rem;
+    width: 30rem;
+    height: 25rem;
+    background-color: crimson;
+    margin: 0 auto;
+}
+</style>
