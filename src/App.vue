3bbbc287e09f70e3a65a7153fa87bc05a37ad3cb
Manuel <mnaldi.business@gmail.com>
2023-05-09 17:49:48 +0200

    fix: change router-link path

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
 <template>
     <div class="navbar">
       <img class="logo" src="/casabondi.svg"/>
-      <a><router-link to="/">Home</router-link></a>
-      <a><router-link to="/calendar">Calendario</router-link></a>
-      <a><router-link to="/contacts">Contatti</router-link></a>
+      <a><router-link to="/biblioteca-castenaso/">Home</router-link></a>
+      <a><router-link to="/biblioteca-castenaso/calendar">Calendario</router-link></a>
+      <a><router-link to="/biblioteca-castenaso/contacts">Contatti</router-link></a>
     </div>
     <br>
     <div class="view">
