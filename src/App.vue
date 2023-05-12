ef8aaeed65830c43bf24ff891f957ad399121e59
Manuel <mnaldi.business@gmail.com>
2023-05-12 12:28:14 +0200

    feat: add fontsawsome in navbar

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,34 @@
   <header>
     <div class="navbar">
       <img class="logo" src="/casabondi.svg"/>
-      <a><router-link to="/biblioteca-castenaso/">Home</router-link></a>
-      <a><router-link to="/biblioteca-castenaso/calendar">Calendario</router-link></a>
-      <a><router-link to="/biblioteca-castenaso/guide">Guida</router-link></a>
-      <a><router-link to="/biblioteca-castenaso/contacts">Contatti</router-link></a>
+
+      <div class="home">
+        <div class="icon-house">
+          <i class="fa-solid fa-house"></i>
+        </div>
+        <a><router-link to="/biblioteca-castenaso/">Home</router-link></a>
+      </div>
+
+      <div class="calendar">
+        <div class="icon-calendar">
+          <i class="fa-solid fa-calendar"></i>
+        </div>
+        <a><router-link to="/biblioteca-castenaso/calendar">Calendario</router-link></a>
+      </div>
+
+      <div class="guide">
+        <div class="icon-guide">
+          <i class="fa-solid fa-book-open"></i>
+        </div>
+        <a><router-link to="/biblioteca-castenaso/guide">Guida</router-link></a>
+      </div>
+      
+      <div class="contacts">
+        <div class="icon-contacts">
+          <i class="fa-solid fa-address-card"></i>
+        </div>
+        <a><router-link to="/biblioteca-castenaso/contacts">Contatti</router-link></a>
+      </div>
     </div>
     <br>
   </header>
@@ -22,6 +46,11 @@
 </template>
 
 <style>
+.icon-house, .icon-calendar, .icon-guide, .icon-contacts{
+  text-align: center;
+  color: black;
+}
+
 .slide-enter-active{
     animation: slide-in 2s ease-in-out;
     animation-delay: -1s;
