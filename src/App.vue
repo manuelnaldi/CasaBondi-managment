644dc6714507eb80c75db44cae6810d66448f316
manuelnaldi <mnaldi.business@gmail.com>
2024-12-06 12:50:51 +0100

    refactor: remove font-awesome & bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,31 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <header>
     <div class="navbar">
 
       <div class="home">
         <div class="icon-house">
-          <i class="fa-solid fa-house"></i>
+          Home
         </div>
         <a><router-link to="/salastudio-castenaso/">Home</router-link></a>
       </div>
 
       <div class="calendar">
         <div class="icon-calendar">
-          <i class="fa-solid fa-calendar"></i>
+          Calendar
         </div>
         <a><router-link to="/salastudio-castenaso/calendar">Calendario</router-link></a>
       </div>
 
       <div class="guide">
         <div class="icon-guide">
-          <i class="fa-solid fa-book-open"></i>
+          Guide
         </div>
         <a><router-link to="/salastudio-castenaso/guide">Guida</router-link></a>
       </div>
       
       <div class="contacts">
         <div class="icon-contacts">
-          <i class="fa-solid fa-address-card"></i>
+          Contacts
         </div>
         <a><router-link to="/salastudio-castenaso/contacts">Contatti</router-link></a>
       </div>
@@ -37,26 +34,8 @@
   </header>
   <main>
     <div class="view">
-      <transition name="slide">
-        <router-view></router-view>
-      </transition>
+      <router-view></router-view>
     </div>
   </main>
 </template>
 
-<style>
-.icon-house, .icon-calendar, .icon-guide, .icon-contacts{
-  text-align: center;
-  color: black;
-}
-
-.slide-enter-active{
-    animation: slide-in 2s ease-in-out;
-    animation-delay: -1s;
-}
-
-@keyframes slide-in{
-    from { top: 100rem }
-    to { top: 0rem }
- }
-</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,17 @@
 <template>
         <div class="card">
             <h1>Benvenuto!</h1>
-            <p>Sito web creato per gli <i>studiosi</i> della bibilioteca di Castenaso che vogliono consultare
+            <p>Sito web creato per gli studiosi della bibilioteca di Castenaso che vogliono consultare
             l'apertura della biblioteca anche negli orari in cui dovrebbe rimanere chiusa.
             <br>
             <br>
             <b><i>Il progetto in questo momento è ancora in fase di sviluppo.</i></b></p>
             <br>
-            <p>Nella sezione <b>calendario</b> <i class="fa-solid fa-calendar"></i> è possibile accedere per consultare i diversi orari di disponibilità!</p>
+            <p>Nella sezione <b>calendario</b> è possibile accedere per consultare i diversi orari di disponibilità!</p>
             <br>
-            <p>Nella sezione <b>guida</b> <i class="fa-solid fa-book-open"></i> puoi trovare informazioni utili!</p>
+            <p>Nella sezione <b>guida</b> puoi trovare informazioni utili!</p>
             <br>
-            <p>Nella sezione <b>contatti</b> <i class="fa-solid fa-address-card"></i> puoi trovare il link al gruppo whatsapp per comunicare le tue disponibilità,
+            <p>Nella sezione <b>contatti</b> puoi trovare il link al gruppo whatsapp per comunicare le tue disponibilità,
             in qualità di volontario, che verranno aggiunte nel calendario su questo sito web.</p>
         </div>
 </template>
