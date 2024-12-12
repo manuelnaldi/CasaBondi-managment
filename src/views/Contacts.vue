49fb548dee79914f4a46dd347868ef16e01948e8
manuelnaldi <mnaldi.business@gmail.com>
2024-12-12 16:08:29 +0100

    refactor: set up contact view

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,5 +1,14 @@
 <template>
-        <div class="container-xl bg-secondary">
-            <h1>Contatti</h1>
+    <div class="grid-container">
+        <div></div>
+        <div class="content text-center">
+            <h1 class="mt-3">Contatti</h1>
+            <p>
+                <h4 class="mt-5">Vuoi rimanere aggiornato su tutte le aperture straordinarie attraverso whatsapp?</h4>
+                <h4 class="mt-3">Entra nel canale dedicato!</h4>
+                <a href="https://whatsapp.com/channel/0029VaBKBM45q08ZHrU94u3f">SALA STUDIO BIBLIOTECA CASA BONDI <i class="bi bi-link-45deg"></i></a>
+            </p>
         </div>
+        <div></div>
+    </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
-        <div class="container-xl bg-secondary">
-            <h1>Sala Studio Castenaso</h1>
+    <div class="grid-container">
+        <div></div>
+        <div class="content text-center">
+            <h1 class="mt-3">Sala Studio Castenaso</h1>
         </div>
+        <div></div>
+    </div>
 </template>
+
+<style lang="scss">
+    .grid-container{
+        position: fixed;
+        display: grid;
+        grid-template-columns: 2rem auto 2rem;
+        grid-template-rows: auto;
+        width: 100%;
+    }
+</style>
